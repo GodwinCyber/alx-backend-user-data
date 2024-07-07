c24a97776391a1388ca72dfa0cd1577340c1edb2
Godwin <godwinchuks032@gmail.com>
2024-07-07 15:55:25 +0100

    Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -97,15 +97,15 @@ def get_db() -> MySQLConnection:
         PERSONAL_DATA_DB_HOST (set the default as “localhost”).
         The database name is stored in PERSONAL_DATA_DB_NAME.
     """
-    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
-    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    connection = mysql.connector.connect(
-        host=db_host,
-        port=3306,
-        user=db_user,
-        password=db_pwd,
-        database=db_name,
+    user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    database = os.getenv("PERSONAL_DATA_DB_NAME", "holberton")
+    db = mysql.connector.connect(
+        user=user,
+        password=password,
+        host=host,
+        database=database,
+        port=3306
     )
-    return connection
+    return db
