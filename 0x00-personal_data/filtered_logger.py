37e83fd56d5077930c6d84b0a2cdfb130cb88cce
Godwin <godwinchuks032@gmail.com>
2024-07-07 07:32:41 +0100

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(
@@ -27,3 +28,30 @@ def filter_datum(
     escaped_separator = re.escape(separator)
     pattern = r'({})=(.*?){}'.format(joined_fields, escaped_separator)
     return re.sub(pattern, r'\1={}{}'.format(redaction, separator), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Update the class to accept a list of strings
+            fields constructor argument.
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Implement the format method to filter values in incoming log records
+            using filter_datum. Values for fields in fields should be filtered.
+            DO NOT extrapolate FORMAT manually. The format method should be
+            less than 5 lines long.
+        """
+        original_message = super(RedactingFormatter, self).format(record)
+        return filter_datum(
+            self.fields, self.REDACTION, original_message, self.SEPARATOR
+        )
