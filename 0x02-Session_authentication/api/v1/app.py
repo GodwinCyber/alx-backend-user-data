b2f4b855be9ce878f0276ac64310abd51c4a6589
Godwin <godwinchuks032@gmail.com>
2024-07-12 02:16:06 +0100

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -73,9 +73,12 @@ def before_request():
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(
+                request
+            ) is None and auth.session_cookie(request):
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
