c2d6bb731d13f7b3da5ea1b4523a26eb47f5df11
Godwin <godwinchuks032@gmail.com>
2024-07-16 12:19:57 +0100

    update before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -75,14 +75,16 @@ def before_request():
             '/api/v1/forbidden/',
             '/api/v1/auth_session/login/'
         ]
-        if auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(
-                request
-            ) is None and auth.session_cookie(request):
-                abort(401)
-            if auth.current_user(request) is None:
-                abort(403)
-            request.current_user = auth.current_user(request)
+        if not auth.require_auth(request.path, excluded_paths):
+            return
+        authorization = auth.authorization_header(request)
+        session = auth.session_cookie(request)
+        if authorization is None and session is None:
+            abort(401)
+        current_user = auth.current_user(request)
+        if current_user is None:
+            abort(403)
+        request.current_user = current_user
 
 
 if __name__ == "__main__":
---
+++ 0x02-Session_authentication/print_env.sh
@@ -0,0 +1,4 @@
+printenv | grep API_HOST
+printenv | grep API_PORT
+printenv | grep AUTH_TYPE
+printenv | grep SESSION_NAME
