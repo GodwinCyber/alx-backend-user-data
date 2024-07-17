d5a3ae73e9f9197bbee9f955f681f51f36ee68e5
Godwin <godwinchuks032@gmail.com>
2024-07-17 22:25:49 +0100

    Destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -114,3 +114,12 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroys a session: In this task, you will implement the:
+            The method updates the corresponding user's session ID to None
+        Note:
+            Only use public methods of self._db
+        """
+        self._db.update_user(user_id, session_id=None)
+        return None
